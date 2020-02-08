a9940f800f361a33cb5b7b6509aae2bd1b6e7ef4
caoxy <caoxy@gongfudou.com>
2020-02-08 16:52:53 +0800

    js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,6 @@
 function copyPassword(e) {
-		var pwd = getPwd();
+	var pwd = getPwd();
+	alert(`get pwd: ${pwd}`);
   	copyToClipboard(pwd);
 }
 
@@ -11,12 +12,15 @@ function getPwd() {
 
 function copyToClipboard (text) {
   var textArea = document.createElement("textarea");
-	textArea.style.opacity = 0;
-	textArea.value = text;
-	document.body.appendChild(textArea);
+  textArea.style.opacity = 0;
+  textArea.value = text;
+  document.body.appendChild(textArea);
   textArea.select();
 
   try {
+  	if (!document.execCommand) {
+  		alert('不支持 execCommand');
+  	}
     var successful = document.execCommand('copy');
     var msg = successful ? '成功复制到剪贴板' : '该浏览器不支持点击复制到剪贴板';
    alert(msg);
