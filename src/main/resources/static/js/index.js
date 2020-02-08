6de732e954c95a82561c0826dd92b2cbb7795ce1
mango <cxy1754575493@gmail.com>
2020-02-08 17:43:08 +0800

    change click

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,37 +1,37 @@
 window.onload = function() {
-  var btnEl = document.getElementById('pwdButton');
-  if (btnEl) {
-    btnEl.addEventListener('click', copyPasswordHandler)
-  }
-  function copyPasswordHandler(e) {
-    var pwd = getPwd();
-    copyToClipboard(pwd);
-  }
-
-  function getPwd() {
-    var pwdEl = document.getElementById('password');
-    var pwd = pwdEl.innerText;
-    return pwd.slice(1, pwd.length - 1);
-  }  
-
-  function copyToClipboard (text) {
-    var textArea = document.createElement("textarea");
-    textArea.style.opacity = 0;
-    textArea.value = text;
-    document.body.appendChild(textArea);
-    textArea.select();
-
-    try {
-      if (!document.execCommand) {
-        alert('不支持 execCommand');
-      }
-      var successful = document.execCommand('copy');
-      var msg = successful ? '成功复制到剪贴板' : '该浏览器不支持点击复制到剪贴板';
-     alert(msg);
-    } catch (err) {
-      alert('该浏览器不支持点击复制到剪贴板');
+    var btnEl = document.getElementById('pwdButton');
+    if (btnEl) {
+        btnEl.addEventListener('click', copyPasswordHandler)
+    }
+    function copyPasswordHandler(e) {
+        var pwd = getPwd();
+        copyToClipboard(pwd);
     }
 
-    document.body.removeChild(textArea);
-  }
+    function getPwd() {
+        var pwdEl = document.getElementById('password');
+        var pwd = pwdEl.innerText;
+        return pwd.slice(1, pwd.length - 1);
+    }
+
+    function copyToClipboard (text) {
+        var textArea = document.createElement("textarea");
+        textArea.style.opacity = 0;
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+
+        try {
+            if (!document.execCommand) {
+                alert('不支持 execCommand');
+            }
+            var successful = document.execCommand('copy');
+            var msg = successful ? '成功复制到剪贴板' : '该浏览器不支持点击复制到剪贴板';
+            alert(msg);
+        } catch (err) {
+            alert('该浏览器不支持点击复制到剪贴板');
+        }
+
+        document.body.removeChild(textArea);
+    }
 }
